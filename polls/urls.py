94f29dab7fe19ff2f548ff566959c6bfc334c49c
CHAIMAA LOUGSARI <49941834+chaimaaloug@users.noreply.github.com>
2024-01-05 16:17:20 +0100

    update front

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,9 @@
 # myproject/urls.py
 from django.contrib import admin
 from django.urls import path  # Make sure to import 'include'
+from .views import prediction
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('prediction/', prediction, name='prediction'),
 ]
