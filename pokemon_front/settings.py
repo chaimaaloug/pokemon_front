b903fbe356901389d08e4fa784529a591225714c
CHAIMAA LOUGSARI <49941834+chaimaaloug@users.noreply.github.com>
2024-01-12 21:13:59 +0100

    import css and images

--- pokemon_front/settings.py
+++ pokemon_front/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -116,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    BASE_DIR / "static"
 ]
 
 MEDIA_URL = '/media/'
