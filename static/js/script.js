27d91d59d04816cc20e079c98770ae89791a68a8
CHAIMAA LOUGSARI <49941834+chaimaaloug@users.noreply.github.com>
2024-01-19 15:45:15 +0100

    send selected data to back

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,12 @@
-
-function getPokemonTypes() {
-    axios.get('http://localhost:8000/get_pokemon_types')
-        .then(function (response) {
-            let types = response.data.pokemon_types;
-            addOptionsToDropdown(types, 'pokemonType1');
-            addOptionsToDropdown(types, 'pokemonType2');
-        })
-        .catch(function (error) {
-            console.error('Erreur lors de la récupération des types de Pokémon', error);
-        });
+async function getPokemonTypes() {
+    try {
+        const response = await axios.get('http://localhost:8000/get_pokemon_types');
+        let types = response.data.pokemon_types;
+        addOptionsToDropdown(types, 'pokemonType1');
+        addOptionsToDropdown(types, 'pokemonType2');
+    } catch (error) {
+        console.error('Erreur lors de la récupération des types de Pokémon', error);
+    }
 }
 
 function addOptionsToDropdown(options, dropdownId) {
@@ -28,12 +26,20 @@ function addOptionsToDropdown(options, dropdownId) {
     }
 }
 
-function validerPokemon() {
-    let selectedType1 = document.getElementById('pokemonType1').value;
-    let selectedType2 = document.getElementById('pokemonType2').value;
+async function validerPokemon() {
+    try {
+        let selectedType1 = document.getElementById('pokemonType1').value;
+        let selectedType2 = document.getElementById('pokemonType2').value;
 
-    console.log('Type de Pokémon 1:', selectedType1);
-    console.log('Type de Pokémon 2:', selectedType2);
+        const response = await axios.post('http://localhost:8000/send_selected_pokemons', {
+            type1: selectedType1,
+            type2: selectedType2
+        });
+        console.log('Selected values:', selectedType1, selectedType2);
+        console.log('Réponse du backend:', response.data);
+    } catch (error) {
+        console.error('Erreur lors de l\'envoi des données au backend', error);
+    }
 }
 
 getPokemonTypes();
